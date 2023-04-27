8639afd3177995e053d4bb038d086694d7166581
EltonCatman <egor.a2004@gmail.com>
2023-04-27 18:31:46 +0300

    Shows dividers of a number that is not prime

--- primenumbers.py
+++ primenumbers.py
@@ -1,8 +1,13 @@
 def is_prime(x):
+    dels = []
     for i in range(2, x//2+1):
         if x%i == 0:
-            return False
-    return True
+            dels.append(i)
+    print(dels)
+    if len(dels) > 0:
+        return False
+    else:
+        return True
 
 while True:
     p = int(input())
